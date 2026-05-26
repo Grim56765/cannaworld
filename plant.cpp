f8bfebf092cc8b5c60727f4ddc6dcdcba61a47e0
Grim56765 <38599437+Grim56765@users.noreply.github.com>
2026-05-26 14:12:14 +0000

    Restructure

--- plant.cpp
+++ plant.cpp
@@ -104,22 +104,3 @@ public:
     }
 };
 
-int main() {
-    Genotype clemmonsGeno;
-
-    std::cout << "=== Clemmons Genetic Line ===\n\n";
-
-    for (int i = 0; i < 5; i++) {
-        Phenotype pheno(clemmonsGeno);
-
-        std::cout << "Plant " << (i + 1) << "\n";
-        std::cout << pheno.phenoGrowthSpeed << " Weeks\n";
-        std::cout << pheno.phenoYield << " Ounces\n";
-        std::cout << "Color: " << pheno.color << "\n";
-        std::cout << "Hidden Color: " << pheno.hiddenColor << "\n";
-        std::cout << pheno.colorExpressed << "\n";
-        std::cout << pheno.gender << "\n\n";
-    }
-
-    return 0;
-}
